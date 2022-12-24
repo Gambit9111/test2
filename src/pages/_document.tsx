3e012587e3e7d8a6408f716b8528e84550c20598
abc <abc@pop-os.localdomain>
2022-12-25 00:16:38 +0200

    4

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,7 @@ class MyDocument extends Document {
 
     render() {
         return (
-            <Html lang="en">
+            <Html>
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
                 </Head>
